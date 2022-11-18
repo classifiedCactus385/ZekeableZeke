61810f8a639b7e7ab2a14edad865e68b22550c2f
classifiedCactus385 <118504749+classifiedCactus385@users.noreply.github.com>
2022-11-18 13:42:36 +0800

    Add files via upload

---
+++ zekonuts.js
@@ -0,0 +1,66 @@
+let urMom = document.getElementById('zeke')
+
+function aljo() {
+	let number = Math.floor(Math.random() * 15)
+	console.log(number)
+	if (number == 0) {
+		urMom.src = "zeke0.png"
+		console.log("I remember when")
+	}
+	if (number == 1) {
+		urMom.src = "zeke1.png"
+		console.log("Hi zeke!")
+	}
+	if (number == 2) {
+		urMom.src = "zeke2.png"
+		console.log("WHAT?!?")
+	}
+	if (number == 3) {
+		urMom.src = "zeke3.png"
+		console.log("Haluh")
+	}
+	if (number == 4) {
+		urMom.src = "zeke4.png"
+		console.log("Ay si RayJ")
+	}
+	if (number == 5) {
+		urMom.src = "zeke5.png"
+		console.log("Alfonso in love")
+	}
+	if (number == 6) {
+		urMom.src = "zeke6.png"
+		console.log("I remember when")
+	}
+	if (number == 7) {
+		urMom.src = "zeke7.png"
+		console.log("I remember when")
+	}
+	if (number == 8) {
+		urMom.src = "zeke8.png"
+		console.log("I remember when")
+	}
+	if (number == 9) {
+		urMom.src = "zeke9.png"
+		console.log("I remember when")
+	}
+	if (number == 10) {
+		urMom.src = "zeke10.png"
+		console.log("I remember when")
+	}
+	if (number == 11) {
+		urMom.src = "zeke11.png"
+		console.log("I remember when")
+	}
+	if (number == 12) {
+		urMom.src = "zeke12.png"
+		console.log("I remember when")
+	}
+	if (number == 13) {
+		urMom.src = "zeke13.png"
+		console.log("I remember when")
+	}
+	if (number == 14) {
+		urMom.src = "zeke14.png"
+		console.log("I remember when")
+	}
+}
